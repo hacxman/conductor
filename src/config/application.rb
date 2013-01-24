083554e02a72dd0b3568e0d54a953b95687974a1
Maros Zatko <mzatko@redhat.com>
2013-01-24 16:37:24 +0100

    fix autoload_paths

--- src/config/application.rb
+++ src/config/application.rb
@@ -38,7 +38,7 @@ module Conductor
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += %W(#{Rails.root}/app/presenters)
+    config.autoload_paths += %W(#{config.root}/app/presenters)
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
