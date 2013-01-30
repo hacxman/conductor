964a523f19fe40d1a5a8f0ecc449c5dec194df4f
Maros Zatko <mzatko@redhat.com>
2013-01-30 17:04:11 +0100

    dirty hack with instance_eval

--- src/app/presenters/deployables_presenter.rb
+++ src/app/presenters/deployables_presenter.rb
@@ -1,6 +1,55 @@
 module DeployablesP
   module ShowP
-    def wui(params)
+    def wui(params, s)
+      s.instance_eval do
+      @deployable = Deployable.find(params[:id])
+      @catalog = params[:catalog_id].present? ? Catalog.find(params[:catalog_id]) : @deployable.catalogs.first
+      require_privilege(Privilege::VIEW, @deployable)
+      save_breadcrumb(polymorphic_path([@catalog, @deployable]), @deployable.name)
+      @providers = Provider.all
+      @catalogs_options = Catalog.list_for_user(current_session, current_user,
+                                                Privilege::MODIFY).select do |c|
+        !@deployable.catalogs.include?(c) and
+          @deployable.catalogs.first.pool_family == c.pool_family
+      end
+
+      if @catalog.present?
+        add_permissions_inline(@deployable, '', {:catalog_id => @catalog.id})
+      else
+        add_permissions_inline(@deployable)
+      end
+
+      @images_details, images, @missing_images, @deployable_errors = @deployable.get_image_details
+      flash.now[:error] = @deployable_errors unless @deployable_errors.empty?
+
+      if @missing_images.empty?
+        @image_status = []
+        @pushed_count = 0
+
+        @deployable.pool_family.provider_accounts.includes(:provider).
+                    where('providers.enabled' => true).each do |provider_account|
+          deltacloud_driver =
+            provider_account.provider.provider_type.deltacloud_driver
+          build_status = @deployable.build_status(images, provider_account)
+          @pushed_count += 1 if (build_status == :pushed)
+
+          @image_status << {
+            :deltacloud_driver => deltacloud_driver,
+            :provider_account_label => provider_account.label,
+            :provider_name => provider_account.provider.name,
+            :build_status => build_status,
+            :translated_build_status =>
+              t("deployables.show.build_statuses_descriptions.#{build_status}")
+          }
+
+          @image_status.sort_by do |image_status_for_account|
+            image_status_for_account[:deltacloud_driver]
+          end
+        end
+      end
+      end
+
+
     end
 
     def api(params)
