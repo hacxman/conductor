208380ad2a1104495cc0dc54f281d357f313b7e5
Maros Zatko <mzatko@redhat.com>
2013-02-06 16:46:58 +0100

    lol

--- src/app/presenters/deployables_presenter.rb
+++ src/app/presenters/deployables_presenter.rb
@@ -4,7 +4,7 @@ module RestrictedPresenter
       p base
       p 'kokot'
       base.class_eval do
-        def included
+        def self.included
           p base
           p 'jebem ti rit'
         end
