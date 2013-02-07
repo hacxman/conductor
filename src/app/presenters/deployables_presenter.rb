4f23c46836f9fab6dadfbf0ed9ea9661bf978a3d
Maros Zatko <mzatko@redhat.com>
2013-02-07 17:03:09 +0100

    fuhacko

--- src/app/presenters/deployables_presenter.rb
+++ src/app/presenters/deployables_presenter.rb
@@ -14,8 +14,8 @@ module RestrictedPresenter
             define_method fname do |*lampa, &block|
               blank_slate = Class.new do
                 instance_methods.each do |m|
-                  undef_method m unless m.to_s =~ /method_missing|respond_to?|^__|^instance_variable|^define_method|^send/
-                  puts "undef_method '#{m.to_s}'" unless m.to_s =~ /method_missing|respond_to?|^__|^instance_variable|^define_method|^send/
+#                  undef_method m unless m.to_s =~ /method_missing|respond_to?|^__|^instance_variable|^define_method|^send/
+#                  puts "undef_method '#{m.to_s}'" unless m.to_s =~ /method_missing|respond_to?|^__|^instance_variable|^define_method|^send/
                 end
               end
 
